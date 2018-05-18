fc1caeeb23ab76b8fd61ee8e90102623d1eb158d
冯鑫龙 <18862005869@163.com>
2018-05-18 15:27:06 +0800

    更新 评论和回复功能

--- app/Modules/Home/Repositories/CommentRepository.php
+++ app/Modules/Home/Repositories/CommentRepository.php
@@ -4,6 +4,7 @@ namespace App\Modules\Home\Repositories;
 
 use App\Modules\Basic\Repositories\BaseRepository;
 use App\Modules\Home\Models\Comment;
+use Illuminate\Http\Request;
 
 
 class CommentRepository extends BaseRepository
@@ -14,6 +15,16 @@ class CommentRepository extends BaseRepository
     public function model()
     {
         return Comment::class;
+    }
+    //获取评论添加的相关数据
+    public function addComRequest(Request $request)
+    {
+        $options = [
+            'content' => $request->get('content',''),
+            'article_id' => $request->get('id',''),
+            'cate' => $request->get('cate',''),
+        ];
+        return $options;
     }
 
 
